8a4d6ae604af4f656c3d05f0cdb486058df31c10
YukiP7 <gargyukti112@gmail.com>
2025-06-28 12:02:47 +0530

    update webpage name

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import resumePdf from "../assets/documents/Yukti-resume.pdf"
+import resumePdf from "../assets/documents/Yukti_resume.pdf"
 
 const About = () => {
   return (
